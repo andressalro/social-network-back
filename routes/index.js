a21e08969ccae718a971787439b183fa9501110a
Andrés Salgado Rojas <asalgado@darumasoftware.com>
2022-02-27 22:56:56 -0500

    Feature: Eliminar rutas V2

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
 export { routerV1 } from "./v1";
-export { routerV2 } from "./v2";
 export { redirectRouter } from "./redirect.routes";
 export { authRouter } from "./auth/auth.router";
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import { ServerConfig } from "./config";
-import { routerV2, routerV1, redirectRouter, authRouter } from "./routes";
+import { routerV1, redirectRouter, authRouter } from "./routes";
 
 async function main() {
     const PORT = process.env.PORT || 3000;
     const server = new ServerConfig({
         port: PORT,
         //middleware: []
-        routers: [redirectRouter, routerV2, routerV1, authRouter]
+        routers: [redirectRouter, routerV1, authRouter]
     });
   server.listen();
 }
